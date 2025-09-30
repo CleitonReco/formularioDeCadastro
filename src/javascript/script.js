6d9b5f5b40070279cf15779348f58bf413ff5f58
CleitonReco <cleiton.silvestre@outlook.com>
2025-09-30 06:24:27 -0300

    Segundo campo Válidado

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,30 +1,71 @@
-const form = document.querySelector('#form');
+const form = document.querySelector("#form");
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault()
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-  const errorIcon = '<i class="fa-solid fa-circle-exclamation"></i>'
+  const fields = [
+    {
+      id: 'name',
+      label: 'Nome',
+      validator: nameIsValid
+    },
+    {
+      id: 'last_name',
+      label: 'Sobrenome',
+      validator: nameIsValid
+    },
+  ];
 
-  const name = document.querySelector('#name');
-  const inputBox = name.closest('.input-box');
-  const nameValue = name.value;
-  
+  const errorIcon = '<i class="fa-solid fa-circle-exclamation"></i>';
 
-  const errorSpan = inputBox.querySelector('.error');
-  errorSpan.innerHTML = '';
+  fields.forEach(function (field) {
+    const input = document.getElementById(field.id);
+    const inputBox = input.closest(".input-box");
+    const inputValue = input.value;
+    const errorSpan = inputBox.querySelector(".error");
+    errorSpan.innerHTML = "";
 
- 
-  
-  if (isEmpty(nameValue)) {
-    errorSpan.innerHTML = `${errorIcon} O campo é obrigatório! `
-    return;
-  }
+    inputBox.classList.remove("invalid");
+    inputBox.classList.add("valid");
 
-})
-
+    const fieldValidator = field.validator(inputValue);
+    if (!fieldValidator.isValid) {
+      errorSpan.innerHTML = `${errorIcon} ${fieldValidator.errorMenssage}`;
+      inputBox.classList.add("invalid");
+      inputBox.classList.remove("valid");
+      return;
+    }
+  });
+});
 
 function isEmpty(value) {
-  return value === ''
+  return value === "";
+}
+
+function nameIsValid(value) {
+  const validator = {
+    isValid: true,
+    errorMenssage: null,
+  };
+
+  if (isEmpty(value)) {
+    validator.isValid = false;
+    validator.errorMenssage = "O campo é obrigatório.";
+    return validator;
+  }
+  const min = 3;
+
+  if (value.length < min) {
+    validator.isValid = false;
+    validator.errorMenssage = `Deve ter no mínimo ${min} caracteres.`;
+    return validator;
+  }
+  const regex = /^[a-zA-Z]/;
+  if (!regex.test(value)) {
+    validator.isValid = false;
+    validator.errorMenssage = "o campo deve ter conter apenas letras!";
+  }
+  return validator;
 }
 
 const passwordIcons = document.querySelectorAll(".password-icon");
